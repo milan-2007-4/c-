3608ff5b4f8e73acb211eb99073b9c6c980f5940
milan ghodasara <milanghodasara73@gmail.com>
2026-03-01 15:51:37 +0530

    Create Sub_sum.cs

---
+++ Sub_sum.cs
@@ -0,0 +1,26 @@
+using System;
+class sub_sum
+{
+    static void Main()
+    {
+        int x = 20;
+        int y = 5;
+
+        com_sub_sum obj = new com_sub_sum();
+        obj.Calculate(ref x, ref y);
+    }
+}
+
+class com_sub_sum
+{
+    public void Calculate(ref int a, ref int b)
+    {
+        int sum = a + b;
+        int sub = a - b;
+
+        Console.WriteLine("Sum = " + sum);
+        Console.WriteLine("Subtraction = " + sub);
+    }
+}
+
+
